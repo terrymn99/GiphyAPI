2afa7743578b4d0ec7b655d672f998139858353d
Terry Kochaver <terrykochaver3@gmail.com>
2018-04-20 17:13:40 -0500

    initial button load working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
-$(document).ready(function () {
+//$(document).ready(function () {
+    $(function ()  {
+        loadButtons ("gifItems", "createButtons", ".buttonGroup");
+    })
+    //Set items array
+    var gifItems = ["German Shepherd", "Poodle", "Pit Bull"]; 
+    //Create buttons
+    function loadButtons (array, classToAdd, areaToAdd) {
+        $(areaToAdd).empty(); //Clear out input box
+        //Loop through array
+        for (var i = 0; i < gifItems.length; i++) {
+            var a = $("<button>"); //Creating button to define
+            a.addClass(classToAdd); //Adding a class to button
+            a.attr("data-type", gifItems[i]); //Adding data type equal to array items
+            a.text(gifItems[i]); //Adding text that will display gif item
+            $(areaToAdd).append (a);  //END LOADBUTTONS FUNCTION
+            
+        }
+    }
 
-
-
-
-});
