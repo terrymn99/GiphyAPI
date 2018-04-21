b91e2310b2386bee7960e76a5bb2a7309f0dfb0a
Terry Kochaver <terrykochaver3@gmail.com>
2018-04-21 02:23:47 -0500

    Gifs displaying, but not properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
         }
     }
 
-    //Create listener event. Execute function when search button is clicked
+    //Create event listener. Execute function when search box is clicked
     $(document).on("click", ".searchArea", function () {
         var type = $(this).data("type");
         //queries Giphy API
@@ -26,8 +26,31 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-        })
-    })
+            console.log (response);
+        //Loops through response data array
+    for (var i =0; i < response.data.length; i++) {   
+        var imageSearch = $("<div class = 'image-search'>");  //Creates image-search class, assigns it to imageSearch variable
+        var rating = response.data[i].rating; //Loops through object, acesses rating for each item
+        var p = $("<p>").text("Gif Rating: " +rating); //Creates paragraph to display rating
+        var stillImage = response.data[i].images.fixed_height_still.url; //Loops through response objects accessing still images
+        var animatedImage =response.data[i].images.fixed_height.url; //Loops through response objects accessing animated images
+        var image = $("<img>"); //Assigns image tag to image variable
+        image.attr("src", stillImage);           //adding attributes to image tag
+        image.attr("data-still", stillImage);    //adding attributes to image tag
+        image.attr("data-still", animatedImage); //adding attributes to image tag
+        image.attr("data-state", "stillImage");   //adding attributes to image tag
+        image.addClass("searchImage");   //Add class of searchImage
+        imageSearch.append(p);  //Display rating for each image
+        imageSearch.append(image);  //Display images for Gifs
+        $(".searchArea").append (imageSearch);  //Put imageSearch info in html searchArea class
+    }
+})
+})
+$("addSearch").on("click", function () { 
+    var newItem = $("input").eq(0).val; //Assigns input text to newItem variable
+    arrayItems.push (newItem); //Adds input value to items array
+    loadButtons (loadButtons, "searchButton,", "#buttonGroup"); //Execute loadButtons function
+    return false; //Prevents page from reloading
+})
 
 })
